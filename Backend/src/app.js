f54e239972fa17fa8a3e072bd964ac7dad49109a
Rajan <rajanmasih154@gmail.com>
2024-01-12 01:26:22 +0530

    added Routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,4 +15,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Routes Import
+import userRouter from "./routes/User.routes.js";
+
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter)
+
 export { app };
