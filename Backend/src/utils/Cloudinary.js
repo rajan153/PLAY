1f43be79ac185b88748077ea5e7f5aa3bc20907e
Rajan <rajanmasih154@gmail.com>
2024-01-14 23:15:48 +0530

    fixed code

--- Backend/src/utils/Cloudinary.js
+++ Backend/src/utils/Cloudinary.js
@@ -9,7 +9,7 @@ cloudinary.config({
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
-    if (localFilePath) return null;
+    if (!localFilePath) return null;
 
     // upload the file to Cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
@@ -26,4 +26,4 @@ const uploadOnCloudinary = async (localFilePath) => {
   }
 };
 
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
