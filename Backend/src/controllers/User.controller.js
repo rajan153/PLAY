d378b2d3ea148b7d1f19a03872932b5e4d1f07cf
Rajan <rajanmasih154@gmail.com>
2024-01-26 23:37:39 +0530

    update file with correction

--- Backend/src/controllers/User.controller.js
+++ Backend/src/controllers/User.controller.js
@@ -29,7 +29,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
   if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar file is required");
+    throw new ApiError(400, "Avatar file path is required");
   }
 
   // upload them to cloudinary, check avatar
